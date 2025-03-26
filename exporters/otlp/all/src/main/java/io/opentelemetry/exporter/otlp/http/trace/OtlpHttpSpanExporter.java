87cbacf5f22aac2d54839f0f3ff8502c53d419e6
harsh <harsh.mehta@motadata.com>
2025-03-26 16:13:21 +0530

    added method.

--- exporters/otlp/all/src/main/java/io/opentelemetry/exporter/otlp/http/trace/OtlpHttpSpanExporter.java
+++ exporters/otlp/all/src/main/java/io/opentelemetry/exporter/otlp/http/trace/OtlpHttpSpanExporter.java
@@ -235,7 +235,7 @@ public final class OtlpHttpSpanExporter implements SpanExporter {
           }
           catch (Exception exception)
           {
-
+              logger.warning(exception.getMessage());
           }
         }
 
@@ -260,12 +260,12 @@ public final class OtlpHttpSpanExporter implements SpanExporter {
   }
 
 
-  private Map<String, Object> parseAttribute(String attributes) {
+  private static Map<String, Object> parseAttribute(String attributes) {
     ObjectMapper objectMapper = new ObjectMapper();
     Map<String, Object> attributeMap = new HashMap<>();
 
     try {
-      if (attributes != null && !attributes.isEmpty()) {
+      if (!attributes.isEmpty()) {
         // Convert Vert.x JsonArray to Jackson JsonNode
         JsonNode arrayNode = objectMapper.readTree(attributes);
 
